85285d4a5b24c90ae5cf559de3ccf25db5a111a6
mziniakov <Maksim_Ziniakov@epam.com>
2022-07-11 09:24:52 +0300

    lesson-6

--- src/main/java/com/zizitop/course/utils/DynamicLinkedList.java
+++ src/main/java/com/zizitop/course/utils/DynamicLinkedList.java
@@ -28,11 +28,22 @@ public class DynamicLinkedList {
         headIndex++;
     }
 
+    public Node getNode(int idx){
+        Node result = tail;
+        for(int i = 1; i <= idx; i++) {
+            result = result.next;
+        }
+        return result;
+    }
+
     public Object get(int idx){
         return null;
     }
 
     public Object remove(int idx){
+        //Object object = get(idx) - return
+        //getNode(idx - 1).next = getNode(idx + 1)
+        //return object;
         return null;
     }
 
