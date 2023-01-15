dd2d004afefdefb5d416d6edc1840b43bdd98f5c
Mops92 <mihailsolovev1992@gmail.com>
2023-01-15 22:04:42 +0300

    added objects "Town", "Street" and "HouseNumber" for HouseAddress

--- src/main/java/com/zizitop/course/data/model/HouseAddress.java
+++ src/main/java/com/zizitop/course/data/model/HouseAddress.java
@@ -3,24 +3,13 @@ package com.zizitop.course.data.model;
 import java.security.InvalidParameterException;
 
 public class HouseAddress {
-    private String town;
-    private String street;
-    private String number;
-    public HouseAddress(String town, String street, String number){
-        if(town != null && !town.isBlank()){
-            this.town = town;
-        }else{
-            throw new InvalidParameterException("Town: this field cannot be empty");
-        }
-        if (street != null){
-            this.street = street;
-        }else{
-            throw new InvalidParameterException("Street: this field cannot be empty");
-        }
-        if(number != null){
-            this.number = number;
-        }else{
-            throw new InvalidParameterException("Number: this field cannot be empty");
-        }
+    private Town town;
+    private Street street;
+    private HouseNumber number;
+    public HouseAddress(Town town, Street street, HouseNumber number){
+        this.town = town;
+        this.street = street;
+        this.number = number;
+
     }
 }
