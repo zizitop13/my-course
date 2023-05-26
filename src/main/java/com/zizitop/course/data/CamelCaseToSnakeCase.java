311276d4d4152aa6d9b168d17350459f27ec2d63
Mops92 <mihailsolovev1992@gmail.com>
2023-05-26 10:53:04 +0300

    converted snake to camel

--- src/main/java/com/zizitop/course/data/CamelCaseToSnakeCase.java
+++ src/main/java/com/zizitop/course/data/CamelCaseToSnakeCase.java
@@ -1,15 +1,40 @@
 package com.zizitop.course.data;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import static java.lang.Character.toUpperCase;
+
 public class CamelCaseToSnakeCase {
-    public static String convert(String camelCase) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < camelCase.length(); i++) {
-            char currentChar = camelCase.charAt(i);
-            if (Character.isUpperCase(currentChar)) {
-                stringBuilder.append("_");
-            }
-            stringBuilder.append(Character.toLowerCase(currentChar));
+    public static String convert(String snakeCase) {
+        char[] snake = snakeCase.toCharArray();
+        LinkedList<Character> camel = new LinkedList<>();
+
+        for (int i = 0; i < snake.length; i++) {
+            camel.add(snake[i]);
         }
-        return stringBuilder.toString();
-    }
+
+        for (int i = 0; i < camel.size(); i++) {
+
+
+            if (camel.get(i) == '_') {
+                camel.remove(i);
+                if (i < camel.size()) {
+                    Character nextChar = toUpperCase(camel.get(i));
+                    camel.set(i, nextChar);
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (Character c : camel) {
+                sb.append(c);
+            }
+
+            String result = sb.toString();
+
+            return result;
+
+        }
+
 }
