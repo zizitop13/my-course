563b6cd97cf098f48b002c31506a25832fcc6d6c
mziniakov <mziniakov@ru.estee.com>
2023-07-01 10:44:36 +0300

    lesson 8 - spring boot

--- lessons/product/lesson-8-spring-boot.md
+++ lessons/product/lesson-8-spring-boot.md
@@ -22,6 +22,7 @@
   - OneToMany
 - JUnit
 - Docker
+- Streams
 
 
 
--- src/main/java/com/zizitop/course/SpringBootMain.java
+++ src/main/java/com/zizitop/course/SpringBootMain.java
@@ -3,10 +3,23 @@ package com.zizitop.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 public class SpringBootMain {
 
     public static void main(String[] args) {
+        Map<String, Double> map = new HashMap<>();
+        List<?> list = new ArrayList<Number>();
+
+        int i = new Integer(0);
+
+
+        map.put("te", new Double(0.0));
+        map.put("te", 3.14);
         SpringApplication.run(SpringBootMain.class, args);
     }
 
